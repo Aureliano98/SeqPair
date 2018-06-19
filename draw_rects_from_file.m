cd4582deb957cf9489d63e47bec67bcf4edc5185
Aureliano98 <rpgmakerxp@126.com>
2018-06-19 08:28:12 +0800

    Delete draw_rects_from_file.m

--- draw_rects_from_file.m
+++
@@ -1,8 +0,0 @@
-function draw_rects_from_file(rect_name, net_name)
-%DRAW_RECTS_FROM_FILE 此处显示有关此函数的摘要
-%   此处显示详细说明
-rects = load(rect_name);
-nets = int32(load(net_name));
-draw_rects(rects, nets);
-end
-
